365f6e8ba24c8acb688227a975c574cc84514b61
SenHan <34976706+senhan07@users.noreply.github.com>
2025-10-11 20:31:58 +0700

    remove short name label match delete_series

--- main.py
+++ main.py
@@ -259,13 +259,12 @@ def delete_series(target_id):
 
     instance = target.get('instance')
     module = target.get('module')
-    short_name = target.get('short_name')
     delete_url = f'{prometheus_address}/api/v1/admin/tsdb/delete_series'
     cleanup_url = f'{prometheus_address}/api/v1/admin/tsdb/clean_tombstones'
 
     try:
         # Delete the series
-        response = requests.post(delete_url, data={'match[]': f'{{instance="{instance}",module="{module}",short_name="{short_name}"}}'})
+        response = requests.post(delete_url, data={'match[]': f'{{instance="{instance}",module="{module}"}}'})
         if response.status_code != 204:
             return {'success': False, 'error': f'Error deleting series from Prometheus: {response.text}'}
 
